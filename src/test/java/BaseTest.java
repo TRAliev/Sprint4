129161eb50307c846c8f708616b8a850ce09ed80
Turpal Aliev <alievturpal.r@gmail.com>
2024-06-20 17:38:43 +0300

    Initial commit with new history

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @Before
+    public void beforeTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+}
